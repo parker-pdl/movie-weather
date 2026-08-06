af61f55532b2ce863b097fbb7b8b77746ee1090c
Parker Data Link <parkerp2020@gmail.com>
2026-08-05 21:45:35 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import { register } from './registerServiceWorker';
+import applySeasonalFavicon from './helpers/seasonalFavicon';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 register();
+applySeasonalFavicon();
